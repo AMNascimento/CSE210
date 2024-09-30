28d2242185ebab98daa636139204d7fb0ad0510e
Anderson <am2nascimento@gmail.com>
2024-09-30 20:48:45 -0300

    Complete Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Senior Solutions Architect";
+        job1._company = "IBM";
+        job1._startYear = 2009;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Staf Integration Engineer";
+        job2._company = "FactoryFix";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Anderson Nascimento";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine(job1._company);
+        // Console.WriteLine(job2._company);
+        // job1.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+        myResume.DisplayResumeDetails();
     }
 }
