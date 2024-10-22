f84772874f68b28baec02400a1d20cea6a1ad883
Anderson <am2nascimento@gmail.com>
2024-10-22 20:06:01 -0300

    develope Journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,24 +1,46 @@
+using System.IO;
+
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
 
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
 
     public void DisplayALl()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveToFile(string file)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}__$__{entry._promptText}__$__{entry._entryText}");
+            }
+        }
     }
 
     public void LoadFromFile(string file)
     {
+        _entries.Clear();
+        string[] lines = System.IO.File.ReadAllLines(file);
 
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("__$__");
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._promptText = parts[1];
+            entry._entryText = parts[2];
+            AddEntry(entry);
+        }
     }
 }
