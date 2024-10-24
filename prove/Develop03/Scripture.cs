53889aa28fab1d663e18b68674d31679e00cc891
Anderson <am2nascimento@gmail.com>
2024-10-24 18:46:20 -0300

    W3: develop Scripture.cs

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -6,21 +6,70 @@ public class Scripture
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
+        string[] words = text.Split(" ");
+
+        foreach (string w in words)
+        {
+            Word word = new Word(w);
+            _words.Add(word);
+        }
     }
 
     public void HideRandomWords(int numberToHide)
     {
-
+        
+        for (int i = 0; i < numberToHide; i++)
+        {
+            List<int> visibleWords = ListVisibleWords();
+            Random random = new Random();
+            int index = random.Next(visibleWords.Count);
+            if (visibleWords.Count > 0)
+            {
+                _words[visibleWords[index]].Hide();                
+            }
+        }
     }
 
     public string GetDisplayText()
     {
-        return "";
+        string text = "";
+        foreach (Word word in _words)
+        {
+            if (text == "")
+            {
+                text += word.GetDisplayText();
+            } else
+            {
+                text += $" {word.GetDisplayText()}";
+            }
+        }
+        return $"{_reference.GetDisplayText()} {text}";
     }
 
     public bool IsCompletlyHidden()
     {
-        return false;
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<int> ListVisibleWords()
+    {
+        List<int> visibleWords = new List<int>();
+        for (int i = 0; i < _words.Count; i++)
+        {
+            if (!_words[i].IsHidden())
+            {
+                visibleWords.Add(i);
+            }
+        }
+
+        return visibleWords;
     }
 
 
