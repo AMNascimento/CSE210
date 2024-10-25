220f9ed82d2fc263464e935d3d9d9f4c75fa9ef0
Anderson <am2nascimento@gmail.com>
2024-10-25 20:02:40 -0300

    W5: complete design activity

---
+++ prove/Develop06/ListingActivity.cs
@@ -0,0 +1,26 @@
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts = new List<string>();
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+        
+    }
+
+    private void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        List<string> list = new List<string>();
+        return list;
+    }
+}
