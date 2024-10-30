de27513121bfaf060fe316504f01f28ee211e2b7
Anderson <am2nascimento@gmail.com>
2024-10-29 23:54:52 -0300

    W6:develop Goal.cs

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -1,8 +1,8 @@
 public abstract class Goal
 {
-    private string _shortName;
-    private string _description;
-    private int _points;
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
 
     public Goal(string name, string description, int points)
     {
@@ -11,14 +11,25 @@ public abstract class Goal
         _points = points;
     }
 
-    public abstract void RecordEvent();
+    public abstract int RecordEvent();
 
     public abstract bool IsComplete();
 
     public virtual string GetDetailsString()
     {
-        return "";
+        if (IsComplete())
+        {
+            return $"[X] {_shortName} ({_description})";
+        } else
+        {
+            return $"[ ] {_shortName} ({_description})";
+        }
     }
 
     public abstract string GetStringRepresentation();
+
+    public string GetName()
+    {
+        return _shortName;
+    }
 }
