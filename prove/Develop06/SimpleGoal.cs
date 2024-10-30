9a8587226f71b23f117288beced267b0cb545b5f
Anderson <am2nascimento@gmail.com>
2024-10-29 23:56:30 -0300

    W6:develop SimpleGoal.cs

--- prove/Develop06/SimpleGoal.cs
+++ prove/Develop06/SimpleGoal.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 public class SimpleGoal : Goal
 {
     private bool _isComplete;
@@ -7,18 +9,30 @@ public class SimpleGoal : Goal
         _isComplete = false;
     }
 
-    public override void RecordEvent()
+    public override int RecordEvent()
     {
-
+        if (_isComplete)
+        {
+            return 0;
+        } else
+        {
+            MarkComplete();
+            return _points;    
+        }
     }
 
     public override bool IsComplete()
     {
-        return false;
+        return _isComplete;
     }
 
     public override string GetStringRepresentation()
     {
-        return "";
+        return $"SimpleGoal:{_shortName},{_description},{_points},{IsComplete()}";
+    }
+
+    public void MarkComplete()
+    {
+        _isComplete = true;
     }
 }
